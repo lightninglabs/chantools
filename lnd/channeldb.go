1a434461fad74aebcf3bd5e408efadbd46979475
peicuiping <ezc5@sina.cn>
2024-12-31 14:45:14 +0800

    Fix function name in comment
    Signed-off-by: peicuiping <ezc5@sina.cn>

--- lnd/channeldb.go
+++ lnd/channeldb.go
@@ -467,7 +467,7 @@ func (b *backend) PrintStats() string {
 	return "n/a"
 }
 
-// filesExists reports whether the named file or directory exists.
+// fileExists reports whether the named file or directory exists.
 func fileExists(name string) bool {
 	if _, err := os.Stat(name); err != nil {
 		if os.IsNotExist(err) {
